82c8a9900e53c32398bb28a9ad3f605d76d8b852
saadriaz999 <saadriaz999@gmail.com>
2022-10-28 23:50:34 +0500

    Completed ButtonBoard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import *
 from utils.HelperFunctions import HelperFunctions
+from utils.ButtonBoard import ButtonBoard
 
 # setting display caption
 pygame.display.set_caption('Pyaint')
@@ -9,6 +10,9 @@ WIN = pygame.display.set_mode((WIDTH + RIGHT_TOOLBAR_WIDTH, HEIGHT))
 
 # initializing the grid stack (layers of canvas)
 GRID_STACK = GridStack(ROWS, COLS, BG_COLOR)
+
+# initializing a button board to keep track of all buttons
+BUTTON_BOARD = ButtonBoard(GRID_STACK)
 
 # creating an object to access helper functions
 HF = HelperFunctions()
@@ -26,94 +30,6 @@ SELECTED_LAYERS = []
 CURRENT_LAYER = 0
 grid = GRID_STACK.grid_stack[CURRENT_LAYER]
 
-
-# Adding Buttons
-buttons = []
-
-# BRUSH_WIDTHS = [
-#     Button(RIGHT_TOOLBAR_CENTER - SIZE_SMALL / 2, 480, SIZE_SMALL, SIZE_SMALL, drawing_color, None, "ellipse"),
-#     Button(RIGHT_TOOLBAR_CENTER - SIZE_MEDIUM / 2, 510, SIZE_MEDIUM, SIZE_MEDIUM, drawing_color, None, "ellipse"),
-#     Button(RIGHT_TOOLBAR_CENTER - SIZE_LARGE / 2, 550, SIZE_LARGE, SIZE_LARGE, drawing_color, None, "ellipse")
-# ]
-
-# adding brush width buttons
-small_brush_width_button = Button(RIGHT_TOOLBAR_CENTER - SIZE_SMALL / 2, 480, SIZE_SMALL, SIZE_SMALL,
-                                  BLACK, None, shape="ellipse", name='small_width')
-medium_brush_width_button = Button(RIGHT_TOOLBAR_CENTER - SIZE_MEDIUM / 2, 510, SIZE_MEDIUM, SIZE_MEDIUM,
-                                   drawing_color, None, shape="ellipse", name='medium_width')
-large_brush_width_button = Button(RIGHT_TOOLBAR_CENTER - SIZE_LARGE / 2, 550, SIZE_LARGE, SIZE_LARGE,
-                                  drawing_color, None, shape="ellipse", name='large_width')
-
-buttons.append(small_brush_width_button)
-buttons.append(medium_brush_width_button)
-buttons.append(large_brush_width_button)
-
-# top layer of color buttons
-for i in range(int(len(COLORS) / 2)):
-    buttons.append(Button(100 + MEDIUM_BUTTON_SPACE * i, BUTTON_Y_TOP_ROW - 14, MEDIUM_BUTTON_WIDTH,
-                          MEDIUM_BUTTON_HEIGHT, COLORS[i]))
-
-# bottom layer of colour buttons
-for i in range(int(len(COLORS) / 2)):
-    buttons.append(Button(100 + MEDIUM_BUTTON_SPACE * i, BUTTON_Y_BOT_ROW - 14, MEDIUM_BUTTON_WIDTH,
-                          MEDIUM_BUTTON_HEIGHT, COLORS[i + int(len(COLORS) / 2)]))
-
-# button to erase from canvas
-erase_button = Button(WIDTH - 2 * LARGE_BUTTON_SPACE, BUTTON_Y_TOP_ROW, LARGE_BUTTON_WIDTH, LARGE_BUTTON_HEIGHT,
-                      WHITE, "Erase", BLACK)
-buttons.append(erase_button)
-
-# button to clear canvas
-clear_button = Button(WIDTH - LARGE_BUTTON_SPACE, BUTTON_Y_TOP_ROW, LARGE_BUTTON_WIDTH, LARGE_BUTTON_HEIGHT,
-                      WHITE, "Clear", BLACK)
-buttons.append(clear_button)
-
-# fill bucket button
-fill_bucket_button = Button(WIDTH - 3 * LARGE_BUTTON_SPACE - 10, BUTTON_Y_TOP_ROW, LARGE_BUTTON_WIDTH,
-                            LARGE_BUTTON_HEIGHT, name="FillBucket", image_url="assets/paint-bucket.png")
-buttons.append(fill_bucket_button)
-
-# button showing the current chosen button
-draw_button = Button(5, HEIGHT - TOOLBAR_HEIGHT / 2 - 30, 60, 60, drawing_color)
-buttons.append(draw_button)
-
-# arrow button to show which layer is currently viewing
-arrow_button = Button(RIGHT_TOOLBAR_CENTER + 32, 40, 12, 24, BLACK, name='arrow', shape='left_arrow')
-buttons.append(arrow_button)
-
-# button to view entire stack
-view_button = Button(RIGHT_TOOLBAR_CENTER - 10, 30, LARGE_BUTTON_WIDTH, LARGE_BUTTON_HEIGHT, TEAL, 'View', BLACK)
-buttons.append(view_button)
-
-# buttons for individual layers
-layer_buttons = []
-for i in range(GRID_STACK.MAX_LAYERS):
-    layer_buttons.append(Button(RIGHT_TOOLBAR_CENTER - 10, 80 + 50 * i, LARGE_BUTTON_WIDTH, LARGE_BUTTON_HEIGHT,
-                                ORANGE, f'Layer{i + 1}', BLACK))
-buttons.append(layer_buttons[0])
-
-# buttons for selecting the layers
-layer_button_checkboxes = []
-for i in range(GRID_STACK.MAX_LAYERS):
-    layer_button_checkboxes.append(Button(RIGHT_TOOLBAR_CENTER - 30, 93 + 50 * i, SMALL_BUTTON_WIDTH,
-                                          SMALL_BUTTON_HEIGHT, RED, '', BLACK, name=f'Checkbox Layer {i + 1}'))
-buttons.append(layer_button_checkboxes[0])
-
-# button for adding layers
-add_button = Button(RIGHT_TOOLBAR_CENTER - 10, 130, LARGE_BUTTON_WIDTH, LARGE_BUTTON_HEIGHT, FUCHSIA, '+', BLACK)
-buttons.append(add_button)
-
-# button for deleting layers
-delete_button = Button(RIGHT_TOOLBAR_CENTER - 48, 350, MEDIUM_BUTTON_WIDTH, MEDIUM_BUTTON_HEIGHT, GRAY, 'Delete', BLACK)
-buttons.append(delete_button)
-
-# button for merging layers
-merge_button = Button(RIGHT_TOOLBAR_CENTER - 16, 350, MEDIUM_BUTTON_WIDTH, MEDIUM_BUTTON_HEIGHT, GRAY, 'Merge', BLACK)
-buttons.append(merge_button)
-
-# button for swapping layers
-swap_button = Button(RIGHT_TOOLBAR_CENTER + 16, 350, MEDIUM_BUTTON_WIDTH, MEDIUM_BUTTON_HEIGHT, GRAY, 'Swap', BLACK)
-buttons.append(swap_button)
 
 # run infinite loop
 while run:
@@ -150,7 +66,7 @@ while run:
             except IndexError:
 
                 # loop through button list to check if user clicked any button
-                for button in buttons:
+                for button in BUTTON_BOARD.buttons:
 
                     # do nothing if no button was clicked
                     if not button.clicked(pos):
@@ -160,7 +76,7 @@ while run:
                     if button.text == "Clear":
                         grid = HF.init_grid(ROWS, COLS, BG_COLOR)
                         drawing_color = BLACK
-                        draw_button.color = drawing_color
+                        BUTTON_BOARD.draw_button.color = drawing_color
                         STATE = "COLOR"
                         break
 
@@ -171,12 +87,12 @@ while run:
 
                     # if add layer button was clicked
                     if button.text == '+':
-                        buttons.append(layer_buttons[GRID_STACK.num_layers])
-                        buttons.append(layer_button_checkboxes[GRID_STACK.num_layers])
+                        BUTTON_BOARD.buttons.append(BUTTON_BOARD.layer_buttons[GRID_STACK.num_layers])
+                        BUTTON_BOARD.buttons.append(BUTTON_BOARD.layer_button_checkboxes[GRID_STACK.num_layers])
                         GRID_STACK.add_layer()
-                        add_button.y += 50
+                        BUTTON_BOARD.add_button.y += 50
                         if GRID_STACK.num_layers == 5:
-                            buttons.remove(add_button)
+                            BUTTON_BOARD.buttons.remove(BUTTON_BOARD.add_button)
                         break
 
                     # if view button was clicked
@@ -195,10 +111,10 @@ while run:
                         layer_clicked = int(button.name[-1]) - 1
                         if layer_clicked in SELECTED_LAYERS:
                             SELECTED_LAYERS.remove(layer_clicked)
-                            layer_button_checkboxes[layer_clicked].color = RED
+                            BUTTON_BOARD.layer_button_checkboxes[layer_clicked].color = RED
                         else:
                             SELECTED_LAYERS.append(layer_clicked)
-                            layer_button_checkboxes[layer_clicked].color = GREEN
+                            BUTTON_BOARD.layer_button_checkboxes[layer_clicked].color = GREEN
                         break
 
                     # if the delete button was clicked
@@ -208,8 +124,9 @@ while run:
 
                         # removing extra layer buttons and their checkmarks
                         for i in range(len(SELECTED_LAYERS)):
-                            buttons.remove(layer_buttons[GRID_STACK.num_layers - 1 - i])
-                            buttons.remove(layer_button_checkboxes[GRID_STACK.num_layers - 1 - i])
+                            BUTTON_BOARD.buttons.remove(BUTTON_BOARD.layer_buttons[GRID_STACK.num_layers - 1 - i])
+                            BUTTON_BOARD.buttons.remove(BUTTON_BOARD.layer_button_checkboxes
+                                                        [GRID_STACK.num_layers - 1 - i])
 
                         # delete layer from grid_stack object
                         SELECTED_LAYERS.sort(reverse=True)
@@ -218,10 +135,10 @@ while run:
 
                         # adding the add-layer button
                         if SELECTED_LAYERS:
-                            add_button.y = 80 + 50 * GRID_STACK.num_layers
+                            BUTTON_BOARD.add_button.y = 80 + 50 * GRID_STACK.num_layers
 
                         # unchecking all checkbox buttons
-                        for check_button in layer_button_checkboxes:
+                        for check_button in BUTTON_BOARD.layer_button_checkboxes:
                             check_button.color = RED
 
                         # unchecking all selected layers in from the backend and setting current layer
@@ -231,7 +148,8 @@ while run:
 
                         # adding the add button to button list if number of layers less than 5
                         if GRID_STACK.num_layers < 5:
-                            buttons.append(add_button)
+                            if BUTTON_BOARD.add_button not in BUTTON_BOARD.buttons:
+                                BUTTON_BOARD.buttons.append(BUTTON_BOARD.add_button)
 
                         break
 
@@ -242,8 +160,9 @@ while run:
 
                         # removing extra layer buttons and their checkmarks
                         for i in range(len(SELECTED_LAYERS) - 1):
-                            buttons.remove(layer_buttons[GRID_STACK.num_layers - 1 - i])
-                            buttons.remove(layer_button_checkboxes[GRID_STACK.num_layers - 1 - i])
+                            BUTTON_BOARD.buttons.remove(BUTTON_BOARD.layer_buttons[GRID_STACK.num_layers - 1 - i])
+                            BUTTON_BOARD.buttons.remove(BUTTON_BOARD.layer_button_checkboxes
+                                                        [GRID_STACK.num_layers - 1 - i])
 
                         # merging all layers
                         top_layer = min(SELECTED_LAYERS)
@@ -257,10 +176,10 @@ while run:
 
                         # adding the add-layer button
                         if SELECTED_LAYERS:
-                            add_button.y = 80 + 50 * GRID_STACK.num_layers
+                            BUTTON_BOARD.add_button.y = 80 + 50 * GRID_STACK.num_layers
 
                         # unchecking all checkbox buttons
-                        for check_button in layer_button_checkboxes:
+                        for check_button in BUTTON_BOARD.layer_button_checkboxes:
                             check_button.color = RED
 
                         # unchecking all selected layers in from the backend and setting current layer
@@ -270,7 +189,8 @@ while run:
 
                         # adding the add button to button list if number of layers less than 5
                         if GRID_STACK.num_layers < 5:
-                            buttons.append(add_button)
+                            if BUTTON_BOARD.add_button not in BUTTON_BOARD.buttons:
+                                BUTTON_BOARD.buttons.append(BUTTON_BOARD.add_button)
 
                         break
 
@@ -307,11 +227,13 @@ while run:
 
                     # change the drawing color if any color button was clicked
                     drawing_color = button.color
-                    draw_button.color = drawing_color
+                    BUTTON_BOARD.draw_button.color = drawing_color
+                    BUTTON_BOARD.drawing_color = drawing_color
 
                     break
 
     # draw changes on the canvas
-    HF.draw(WIN, buttons, arrow_button, CURRENT_LAYER, GRID_STACK, SIZE_SMALL, SIZE_MEDIUM, SIZE_LARGE, BRUSH_SIZE)
+    HF.draw(WIN, BUTTON_BOARD.buttons, BUTTON_BOARD.arrow_button, CURRENT_LAYER, GRID_STACK, SIZE_SMALL,
+            SIZE_MEDIUM, SIZE_LARGE, BRUSH_SIZE)
 
 pygame.quit()
---
+++ utils/ButtonBoard.py
@@ -0,0 +1,96 @@
+from utils import *
+
+
+class ButtonBoard:
+
+    def __init__(self, grid_stack):
+        self.grid_stack = grid_stack
+        self.drawing_color = BLACK
+        self.buttons = []
+
+        # top layer of color buttons
+        for i in range(int(len(COLORS) / 2)):
+            self.buttons.append(Button(100 + MEDIUM_BUTTON_SPACE * i, BUTTON_Y_TOP_ROW - 14, MEDIUM_BUTTON_WIDTH,
+                                       MEDIUM_BUTTON_HEIGHT, COLORS[i]))
+
+        # bottom layer of colour buttons
+        for i in range(int(len(COLORS) / 2)):
+            self.buttons.append(Button(100 + MEDIUM_BUTTON_SPACE * i, BUTTON_Y_BOT_ROW - 14, MEDIUM_BUTTON_WIDTH,
+                                       MEDIUM_BUTTON_HEIGHT, COLORS[i + int(len(COLORS) / 2)]))
+
+        # button showing the current chosen color
+        self.draw_button = Button(5, HEIGHT - TOOLBAR_HEIGHT / 2 - 30, 60, 60, self.drawing_color)
+        self.buttons.append(self.draw_button)
+
+        # buttons to choose paint brush width
+        self.small_brush_width_button = Button(RIGHT_TOOLBAR_CENTER - SIZE_SMALL / 2, 480, SIZE_SMALL, SIZE_SMALL,
+                                               BLACK, None, shape="ellipse", name='small_width')
+        self.medium_brush_width_button = Button(RIGHT_TOOLBAR_CENTER - SIZE_MEDIUM / 2, 510, SIZE_MEDIUM, SIZE_MEDIUM,
+                                                self.drawing_color, None, shape="ellipse", name='medium_width')
+        self.large_brush_width_button = Button(RIGHT_TOOLBAR_CENTER - SIZE_LARGE / 2, 550, SIZE_LARGE, SIZE_LARGE,
+                                               self.drawing_color, None, shape="ellipse", name='large_width')
+
+        self.buttons.append(self.small_brush_width_button)
+        self.buttons.append(self.medium_brush_width_button)
+        self.buttons.append(self.large_brush_width_button)
+
+        # button to erase from canvas
+        self.erase_button = Button(WIDTH - 2 * LARGE_BUTTON_SPACE, BUTTON_Y_TOP_ROW, LARGE_BUTTON_WIDTH,
+                                   LARGE_BUTTON_HEIGHT, WHITE, "Erase", BLACK)
+        self.buttons.append(self.erase_button)
+
+        # button to clear canvas
+        self.clear_button = Button(WIDTH - LARGE_BUTTON_SPACE, BUTTON_Y_TOP_ROW, LARGE_BUTTON_WIDTH,
+                                   LARGE_BUTTON_HEIGHT, WHITE, "Clear", BLACK)
+        self.buttons.append(self.clear_button)
+
+        # fill bucket button
+        self.fill_bucket_button = Button(WIDTH - 3 * LARGE_BUTTON_SPACE - 10, BUTTON_Y_TOP_ROW, LARGE_BUTTON_WIDTH,
+                                         LARGE_BUTTON_HEIGHT, name="FillBucket", image_url="assets/paint-bucket.png")
+        self.buttons.append(self.fill_bucket_button)
+
+        # arrow button to show which layer is currently viewing
+        self.arrow_button = Button(RIGHT_TOOLBAR_CENTER + 32, 40, 12, 24, BLACK, name='arrow', shape='left_arrow')
+        self.buttons.append(self.arrow_button)
+
+        # button to view entire stack
+        self.view_button = Button(RIGHT_TOOLBAR_CENTER - 10, 30, LARGE_BUTTON_WIDTH, LARGE_BUTTON_HEIGHT, TEAL, 'View',
+                                  BLACK)
+        self.buttons.append(self.view_button)
+
+        # buttons for individual layers
+        self.layer_buttons = []
+        for i in range(self.grid_stack.MAX_LAYERS):
+            self.layer_buttons.append(Button(RIGHT_TOOLBAR_CENTER - 10, 80 + 50 * i, LARGE_BUTTON_WIDTH,
+                                             LARGE_BUTTON_HEIGHT, ORANGE, f'Layer{i + 1}', BLACK))
+        self.buttons.append(self.layer_buttons[0])
+
+        # buttons for selecting the layers
+        self.layer_button_checkboxes = []
+        for i in range(self.grid_stack.MAX_LAYERS):
+            self.layer_button_checkboxes.append(Button(RIGHT_TOOLBAR_CENTER - 30, 93 + 50 * i, SMALL_BUTTON_WIDTH,
+                                                SMALL_BUTTON_HEIGHT, RED, '', BLACK, name=f'Checkbox Layer {i + 1}'))
+        self.buttons.append(self.layer_button_checkboxes[0])
+
+        # button for adding layers
+        self.add_button = Button(RIGHT_TOOLBAR_CENTER - 10, 130, LARGE_BUTTON_WIDTH, LARGE_BUTTON_HEIGHT, FUCHSIA, '+',
+                                 BLACK)
+        self.buttons.append(self.add_button)
+
+        # button for deleting layers
+        self.delete_button = Button(RIGHT_TOOLBAR_CENTER - 48, 350, MEDIUM_BUTTON_WIDTH, MEDIUM_BUTTON_HEIGHT, GRAY,
+                                    'Delete', BLACK)
+        self.buttons.append(self.delete_button)
+
+        # button for merging layers
+        self.merge_button = Button(RIGHT_TOOLBAR_CENTER - 16, 350, MEDIUM_BUTTON_WIDTH, MEDIUM_BUTTON_HEIGHT, GRAY,
+                                   'Merge', BLACK)
+        self.buttons.append(self.merge_button)
+
+        # button for swapping layers
+        self.swap_button = Button(RIGHT_TOOLBAR_CENTER + 16, 350, MEDIUM_BUTTON_WIDTH, MEDIUM_BUTTON_HEIGHT, GRAY,
+                                  'Swap', BLACK)
+        self.buttons.append(self.swap_button)
+
+    def update_drawing_color(self, color):
+        self.drawing_color = color
